79e574d45ab98f6857f25e0616c63a13c73b9201
HsuShengHui <r12h41006@ntu.edu.tw>
2024-09-07 18:52:44 +0800

    0907 19.

---
+++ src/components/Introduction.js
@@ -0,0 +1,45 @@
+// Introduction.js
+import React from 'react';
+
+function Introduction() {
+    return (
+        <div className="container my-5">
+        {/* Main card */}
+        <div className="card text-center shadow-sm">
+          {/* Card header */}
+          <div className="card-header bg-light">
+            <h2 className="text-primary">藝通台北</h2>
+          </div>
+  
+          {/* Card body */}
+          <div className="card-body">
+            <p className="card-text">
+              探索城市中的藝術美景，藝術體驗台北的獨特魅力！
+              <br />
+              台北市各戶外公共藝術地標，已經超過1000件作品，來為城市帶來活力與創意。
+              <br />
+              若您想探索這些藝術地點，請使用手機定位系統。
+              <br />
+            </p>
+  
+            {/* Notification / warning */}
+            <div className="alert alert-warning" role="alert">
+              本服務將根據您當前的手機定位，找到附近的公共藝術作品。
+              請使用智慧定位服務並進行打卡。
+            </div>
+          </div>
+  
+          {/* Card footer */}
+          <div className="card-footer text-muted">
+            來探索台北這座城市中的藝術瑰寶吧！
+          </div>
+        </div>
+  
+        {/* Map button */}
+        <button className="btn btn-info mt-4 px-4">進入地圖</button>
+      </div>
+    );
+}
+
+export default Introduction;
+
